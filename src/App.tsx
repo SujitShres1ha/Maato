e5c25b39a9c7f1dbf984899c672464fbe87a417d
SujitShres1ha <sthasujit2062@gmail.com>
2025-07-29 08:22:08 -0500

    implement initial App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 
+
+
 function App() {
-
-
   return (
-    <></>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900">Welcome to Maato</h1>
+    </div>
   )
 }
 
